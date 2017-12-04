1de60988d9b74a9dd5fbf5c48a77a5f9b5efd7f1
Mateusz Kociszewski <mateusz1003@gmail.com>
2017-12-04 23:08:17 +0100

    initial commit with lub

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import com.github.steveash.maxclique.Clique;
+import com.github.steveash.maxclique.Cliques;
+import com.github.steveash.maxclique.Weigher;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by mateusz on 04.12.2017.
+ */
+public class Main {
+    public static void main(String[] args ) {
+        List<String> nodesInMyGraph = Arrays.asList("0", "1", "2", "3", "4", "5");
+
+        // need to make an insteance of Weigher<T> which knows how to produce a "weight" (double)
+        // given two nodes (T's) from the graph; any weights <= 0 are treated as negative infinity
+        MyWeigher w = new MyWeigher();
+        w.put("0", "1", 10.0);
+        w.put("1", "2", 10.0);
+        w.put("0", "2", 10.0);
+
+        // find the maximum clique in the graph
+        Clique<String> maximumClique = Cliques.findMaximum(nodesInMyGraph, w);
+
+        // the members of the clique will be in the set: maximumClique.members()
+        System.out.println(maximumClique.members());
+    }
+}
---
+++ src/main/java/main/MyWeigher.java
@@ -0,0 +1,43 @@
+package main;
+
+import com.github.steveash.maxclique.Weigher;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
+/**
+ * @author Steve Ash
+ */
+public class MyWeigher implements Weigher<String> {
+
+    private final Table<String, String, Double> w = HashBasedTable.create();
+
+    public MyWeigher clear() {
+        w.clear();
+        return this;
+    }
+
+    public MyWeigher put(String a, String b, double weight) {
+        if (a.compareTo(b) > 0) {
+            String t = a;
+            a = b;
+            b = t;
+        }
+
+        w.put(a, b, weight);
+        return this;
+    }
+
+    public double weigh(String a, String b) {
+        if (a.compareTo(b) > 0) {
+            String t = a;
+            a = b;
+            b = t;
+        }
+
+        Double maybe = w.get(a, b);
+        if (maybe == null) return -1;
+
+        return maybe;
+    }
+}
+
